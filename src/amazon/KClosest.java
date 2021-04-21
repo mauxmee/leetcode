f0941e2cab9b561fcbed2175d16e6fb8ebb03dbc
mauxmee <mauxmee@gmail.com>
2021-04-21 21:45:18 +1000

    fix compilation errors first

--- src/amazon/KClosest.java
+++ src/amazon/KClosest.java
@@ -1,43 +1,35 @@
 package amazon;
 
-/**     #
- * 146
- *   我们有一个由平面上的点组成的列表 points。需要从中找出 K 个距离原点 (0, 0) 最近的点。
- *
- * （这里，平面上两点之间的距离是欧几里德距离。）
- *
- * 你可以按任何顺序返回答案。除了点坐标的顺序之外，答案确保是唯一的。
- *
- *  
- *
- * 示例 1：
- *
- * 输入：points = [[1,3],[-2,2]], K = 1
- * 输出：[[-2,2]]
- * 解释：
- * (1, 3) 和原点之间的距离为 sqrt(10)，
- * (-2, 2) 和原点之间的距离为 sqrt(8)，
- * 由于 sqrt(8) < sqrt(10)，(-2, 2) 离原点更近。
- * 我们只需要距离原点最近的 K = 1 个点，所以答案就是 [[-2,2]]。
- * 示例 2：
- *
- * 输入：points = [[3,3],[5,-1],[-2,4]], K = 2
- * 输出：[[3,3],[-2,4]]
- * （答案 [[-2,4],[3,3]] 也会被接受。）
- *  
- *
- * 提示：
- *
- * 1 <= K <= points.length <= 10000
- * -10000 < points[i][0] < 10000
- * -10000 < points[i][1] < 10000
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/k-closest-points-to-origin
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ 973. K Closest Points to Origin
+ Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+ The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+ You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+ Example 1:
+
+ Input: points = [[1,3],[-2,2]], k = 1
+ Output: [[-2,2]]
+ Explanation:
+ The distance between (1, 3) and the origin is sqrt(10).
+ The distance between (-2, 2) and the origin is sqrt(8).
+ Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+ We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+ Example 2:
+ Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+ Output: [[3,3],[-2,4]]
+ Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+
+ Constraints:
+ •	1 <= k <= points.length <= 104
+ •	-104 < xi, yi < 104
  */
 public class KClosest {
-    public int[][] kClosest(int[][] points, int k) {
+    public int[][] solution(int[][] points, int k) {
+        return null;
+    }
+
+    public static void main(String[] args) {
 
     }
 }
--- src/amazon/NumIslands.java
+++ src/amazon/NumIslands.java
@@ -1,49 +1,41 @@
 package amazon;
 
-/**  #200
- * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
- *
- * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
- *
- * 此外，你可以假设该网格的四条边均被水包围。
- *
- *  
- *
- * 示例 1：
- *
- * 输入：grid = [
- *   ["1","1","1","1","0"],
- *   ["1","1","0","1","0"],
- *   ["1","1","0","0","0"],
- *   ["0","0","0","0","0"]
- * ]
- * 输出：1
- * 示例 2：
- *
- * 输入：grid = [
- *   ["1","1","0","0","0"],
- *   ["1","1","0","0","0"],
- *   ["0","0","1","0","0"],
- *   ["0","0","0","1","1"]
- * ]
- * 输出：3
- *  
- *
- * 提示：
- *
- * m == grid.length
- * n == grid[i].length
- * 1 <= m, n <= 300
- * grid[i][j] 的值为 '0' 或 '1'
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/number-of-islands
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ 200. Number of Islands
+ Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+ An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ You may assume all four edges of the grid are all surrounded by water.
+
+ Example 1:
+ Input: grid = [
+ ["1","1","1","1","0"],
+ ["1","1","0","1","0"],
+ ["1","1","0","0","0"],
+ ["0","0","0","0","0"]
+ ]
+ Output: 1
+ Example 2:
+ Input: grid = [
+ ["1","1","0","0","0"],
+ ["1","1","0","0","0"],
+ ["0","0","1","0","0"],
+ ["0","0","0","1","1"]
+ ]
+ Output: 3
+
+ Constraints:
+ •	m == grid.length
+ •	n == grid[i].length
+ •	1 <= m, n <= 300
+ •	grid[i][j] is '0' or '1'.
+
  */
 public class NumIslands {
-    class Solution {
-        public int numIslands(char[][] grid) {
+    public int solution(char[][] grid) {
+        return 0;
+    }
 
-        }
+    public static void main(String[] args) {
+
     }
 }
