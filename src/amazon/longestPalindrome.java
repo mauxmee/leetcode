ce84c192a31af560b14ffee4f34d750bd8301f54
mauxmee <mauxmee@gmail.com>
2021-04-25 16:40:12 +1000

    add new questions (not done yet)

---
+++ src/amazon/longestPalindrome.java
@@ -0,0 +1,38 @@
+package amazon;
+
+/**
+ * 5. Longest Palindromic Substring
+ * Given a string s, return the longest palindromic substring in s.
+ * A palindromic number (also known as a numeral palindrome or a numeric palindrome) is a number
+ * (such as 16461) that remains the same when its digits are reversed.
+ * In other words, it has reflectional symmetry across a vertical axis.
+ *
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * Example 3:
+ * Input: s = "a"
+ * Output: "a"
+ *
+ * Example 4:
+ * Input: s = "ac"
+ * Output: "a"
+ *
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters (lower-case and/or upper-case),
+ */
+public class longestPalindrome {
+    public String solution(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String ret = new longestPalindrome().solution("babad");
+    }
+}
