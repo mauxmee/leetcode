350c318af70379fa1cd9cdf12ba141b878b0f3fa
mauxmee <mauxmee@gmail.com>
2021-06-20 18:45:40 +1000

    add multi thread codes

--- src/amazon/isRobotBounded.java
+++ src/amazon/isRobotBounded.java
@@ -1,5 +1,8 @@
 package amazon;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * 1041. Robot Bounded In Circle
  * On an infinite plane, a robot initially stands at (0, 0) and faces north. The robot can receive one of three instructions:
@@ -8,34 +11,74 @@ package amazon;
  * •	"R": turn 90 degrees to the right.
  * The robot performs the instructions given in order, and repeats them forever.
  * Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
- *
- *  Example 1:
+ * <p>
+ * Example 1:
  * Input: instructions = "GGLLGG"
  * Output: true
  * Explanation: The robot moves from (0,0) to (0,2), turns 180 degrees, and then returns to (0,0).
  * When repeating these instructions, the robot remains in the circle of radius 2 centered at the origin.
- *
+ * <p>
  * Example 2:
  * Input: instructions = "GG"
  * Output: false
  * Explanation: The robot moves north indefinitely.
- *
+ * <p>
  * Example 3:
  * Input: instructions = "GL"
  * Output: true
  * Explanation: The robot moves from (0, 0) -> (0, 1) -> (-1, 1) -> (-1, 0) -> (0, 0) -> ...
- *
+ * <p>
  * Constraints:
  * •	1 <= instructions.length <= 100
  * •	instructions[i] is 'G', 'L' or, 'R'.
  */
+
 public class isRobotBounded {
-    public boolean solution(String instructions) {
-        return false;
+
+    class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int instance2() {
+            return x * x + y * y;
+        }
     }
 
-    public static void main(String[] args) {
-        boolean ret = new isRobotBounded().solution("GGLLGG");
 
+    class Direction    {
+        char name;
+        char left;
+        char right;
+
+        public Direction( char n, char l, char r)
+        {
+            name = n;
+            left = l;
+            right = r;
+        }
     }
+
+//    // N: y + 1; S: y - 1; E: x + 1, W: x - 1
+//    // initial direction is E
+//    public boolean solution(String instructions) {
+//        if (instructions == null || instructions.isEmpty()) return true;
+//        int x = 0, y = 0, d = 0, count = 0;
+//        LinkedList<Character> directions = new LinkedList<>(Arrays.asList('E', 'N', 'W', 'S'));
+//        while (count++ < 4) {
+//            for (char d : instructions.toCharArray()) {
+//                if (d)
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public static void main(String[] args) {
+//        boolean ret = new isRobotBounded(direction).solution("GGLLGG");
+//
+//    }
 }
---
+++ src/multiThread/BarrierTest.java
@@ -0,0 +1,109 @@
+package multiThread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BarrierTest {
+    public static class Barrier {
+        private final int numOfWorkers;
+        private Semaphore sem = new Semaphore(0);
+        private int counter = 0;
+        private Lock lock = new ReentrantLock();
+        private AtomicInteger counter2 = new AtomicInteger(0);
+
+        public Barrier(int numOfWorkers) {
+            this.numOfWorkers = numOfWorkers;
+        }
+
+        public void barrier() {
+            lock.lock();
+            boolean isLastWorker = false;
+            try {
+                counter++;
+                if (counter == numOfWorkers) {
+                    isLastWorker = true;
+                }
+
+            } finally {
+                lock.unlock();
+            }
+            if (isLastWorker) {
+                // release num-1 since there are num-1 threads are blocked by the semaphore
+                sem.release(numOfWorkers - 1);
+            } else {
+                try {
+                    sem.acquire();
+                } catch (InterruptedException e) {
+                    System.out.println(Thread.currentThread().getName() + " is interrupted");
+                }
+            }
+        }
+
+        public void barrier2() {
+            boolean isLastWorker = false;
+            if (counter2.incrementAndGet() == numOfWorkers) {
+                isLastWorker = true;
+            }
+
+            if (isLastWorker) {
+                // release num-1 since there are num-1 threads are blocked by the semaphore
+                sem.release(numOfWorkers - 1);
+            } else {
+                try {
+                    sem.acquire();
+                } catch (InterruptedException e) {
+                    System.out.println(Thread.currentThread().getName() + " is interrupted");
+                }
+            }
+        }
+    }
+
+    public static class CoordinatedWorkRunner implements Runnable {
+        private Barrier barrier;
+
+        public CoordinatedWorkRunner(Barrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                task();
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " interrupted");
+            }
+        }
+
+        public void task() throws InterruptedException {
+            System.out.println(Thread.currentThread().getName() + " part 1");
+
+             barrier.barrier();
+            //barrier.barrier2();
+            System.out.println(Thread.currentThread().getName() + " part 2");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int numOfThreads = 12;
+        List<Thread> threads = new ArrayList<>();
+        Barrier barrier = new Barrier(numOfThreads);
+        for (int i = 0; i < numOfThreads; i++) {
+            threads.add(new Thread(new CoordinatedWorkRunner(barrier)));
+        }
+        long start = System.currentTimeMillis();
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t : threads) {
+            t.join();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("used: " + (end-start) + " ms");
+    }
+
+
+}
