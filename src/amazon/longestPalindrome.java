385fa379a236dc6de4683ba319dac4b199b475be
mauxmee <mauxmee@gmail.com>
2021-07-14 22:00:14 +1000

    update algo

--- src/amazon/longestPalindrome.java
+++ src/amazon/longestPalindrome.java
@@ -1,5 +1,7 @@
 package amazon;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * 5. Longest Palindromic Substring
  * Given a string s, return the longest palindromic substring in s.
@@ -29,7 +31,14 @@ package amazon;
  * s consist of only digits and English letters (lower-case and/or upper-case),
  */
 public class longestPalindrome {
-    public String solution(String s) {
+    /*Runtime: 364 ms, faster than 16.94% of Java online submissions for Longest Palindromic Substring.
+Memory Usage: 39.9 MB, less than 37.85% of Java online submissions for Longest Palindromic Substring
+Approach 2: Brute Force
+The obvious brute force solution is to pick all possible starting and ending positions for a substring, and verify if it is a palindrome.
+Complexity Analysis
+Time complexity : O(n^3)Assume that nn is the length of the input string,
+Space complexity : O(1).*/
+    public String bruteForce(String s) {
         String result = s.substring(0, 1);
         // two pointers, one from start, one from end, converge to the middle
         int start = 0, end = s.length() - 1;
@@ -71,12 +80,55 @@ public class longestPalindrome {
         return result;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new longestPalindrome().solution("101234432167"));
-        System.out.println(new longestPalindrome().solution("aaaa"));
-        System.out.println(new longestPalindrome().solution("babad"));
-        System.out.println(new longestPalindrome().solution("abbd"));
-        System.out.println(new longestPalindrome().solution("ab"));
-        System.out.println(new longestPalindrome().solution("b"));
+    /*Runtime: 23 ms, faster than 91.52% of Java online submissions for Longest Palindromic Substring.
+Memory Usage: 39 MB, less than 74.34% of Java online submissions for Longest Palindromic Substring.
+Approach 4: Expand Around Center
+In fact, we could solve it in O(n^2) ) time using only constant space.
+We observe that a palindrome mirrors around its center. Therefore, a palindrome can be expanded from its center,
+ and there are only 2n - 1 such centers.
+You might be asking why there are 2n - 1 but not n centers? The reason is the center of a palindrome can be
+ in between two letters. Such palindromes have even number of letters (such as "abba") and its center are between the two 'b's.*/
+    public String expandAroundCenter(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+    @Test
+    public void test_bruteForce() {
+        System.out.println(bruteForce("101234432167"));
+        System.out.println(bruteForce("aaaa"));
+        System.out.println(bruteForce("babad"));
+        System.out.println(bruteForce("abbd"));
+        System.out.println(bruteForce("ab"));
+        System.out.println(bruteForce("b"));
+    }
+
+    @Test
+    public void test_expandAroundCenter() {
+        System.out.println(expandAroundCenter("101234432167"));
+        System.out.println(expandAroundCenter("aaaa"));
+        System.out.println(expandAroundCenter("babad"));
+        System.out.println(expandAroundCenter("abbd"));
+        System.out.println(expandAroundCenter("ab"));
+        System.out.println(expandAroundCenter("b"));
     }
 }
