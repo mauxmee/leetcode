6753b7418f83f9cbf6f9c9892a0fe89594c7b3d2
mauxmee <mauxmee@gmail.com>
2021-07-22 22:18:57 +1000

    update amazon questions.

---
+++ src/amazon/validParentheses.java
@@ -0,0 +1,45 @@
+package amazon;
+
+/*
+* 20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+Example 4:
+
+Input: s = "([)]"
+Output: false
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+通过次数699,437提交次数1,577,218
+* */
+public class validParentheses {
+    public boolean isValid(String s) {
+        return false;
+    }
+}
