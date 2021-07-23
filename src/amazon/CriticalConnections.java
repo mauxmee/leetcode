9afa79b2e29af674c23bcc78091a19e327b7afad
Kai Xue <Kai.Xue@nasdaq.com>
2021-07-23 18:12:53 +1000

    done two quiz

--- src/amazon/CriticalConnections.java
+++ src/amazon/CriticalConnections.java
@@ -1,14 +1,18 @@
 package amazon;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /*
 * 1192. Critical Connections in a Network
 难度困难
-There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
+There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network
+* where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach other
+* servers directly or indirectly through the network.
 A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
 Return all critical connections in the network in any order.
 
@@ -18,34 +22,155 @@ Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
 Output: [[1,3]]
 Explanation: [[3,1]] is also accepted.
 */
-public class CriticalConnections {
-    static class Conn {
-        public int a, b;
-        public boolean checked = false;
-        public boolean critical = false;
+public class CriticalConnections
+{
+	// This is Tarjan algo for the graph, see
+	// https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+	// time complexity: O( Number of nodes +number of edges)
+	// considering the flags (desc and low ) have O(1) access since they are array
 
-        public Conn(int a, int b) {
-            this.a = Math.min(a, b);
-            this.b = Math.max(a, b);
-        }
+	/*Basically, it uses dfs to travel through the graph to find if current vertex u,
+	can travel back to u or previous vertex
+low[u] records the lowest vertex u can reach
+disc[u] records the time when u was discovered*/
 
-        public Conn(List<Integer> nodes) {
-            if (nodes != null && nodes.size() == 2) {
-                a = Math.min(nodes.get(0), nodes.get(1));
-                b = Math.max(nodes.get(0), nodes.get(1));
-            }
-        }
-    }
+	/*Runtime: 110 ms, faster than 70.87% of Java online submissions for Critical Connections in a Network.
+Memory Usage: 114.8 MB, less than 61.35% of Java online submissions for Critical Connections in a Network.*/
+	public List<List<Integer>> solution( int n, List<List<Integer>> connections )
+	{
+        /*Disc: This indicates the time for when a particular node is discovered or visited during DFS traversal.
+        For each node we increase the Disc time value by 1.
 
-    public List<List<Integer>> solution(int n, List<List<Integer>> connections) {
-        Map<Integer, List<Conn>> routes = new HashMap<>();
-        connections.forEach(nodes -> {
-            Conn conn = new Conn(nodes);
-            routes.computeIfAbsent(conn.a, (k) -> new ArrayList<>()).add(conn);
-        });
-        routes.keySet().forEach(a -> {
+		Low: This indicates the node with lowest discovery time accessible from a given node. If there is a back edge then
+		we update the low value based on some conditions. The maximum value Low for a node can be assigned is equal to the
+		Disc value of that node since the minimum discovery time for a node is the time to visit/discover itself.
+		Note: The Disc value once assigned will not change while we keep on updating the low value traversing each node.
+		We will discuss the condition next.
+		追溯值
+		追溯值用来表示从当前节点 x 作为搜索树的根节点出发，能够访问到的所有节点中，时间戳最小的值 —— low[x]。
+		那么，我们要限定下什么是“能够访问到的所有节点”？，其需要满足下面的条件之一即可：
 
-        });
-        return null;
-    }
+		以 x 为根的搜索树的所有节点
+		通过一条非搜索树上的边，能够到达搜索树的所有节点
+		为*/
+		int[] disc = new int[ n ], low = new int[ n ];
+		Arrays.fill( disc, -1 ); // use disc to track if visited (disc[i] == -1)
+
+		List<Integer>[] graph = new ArrayList[ n ];
+		for ( int i = 0; i < n; i++ )
+		{
+			graph[ i ] = new ArrayList<>();
+		}
+		// build graph
+		for ( int i = 0; i < connections.size(); i++ )
+		{
+			int from = connections.get( i ).get( 0 );
+			int to = connections.get( i ).get( 1 );
+			graph[ from ].add( to );
+			graph[ to ].add( from );
+		}
+		// result
+		List<List<Integer>> res = new ArrayList<>();
+		for ( int i = 0; i < n; i++ )
+		{
+			if ( disc[ i ] == -1 )
+			{
+				dfs( i, low, disc, graph, res, i );
+			}
+		}
+		return res;
+	}
+
+	//时间戳是用来标记图中每个节点在进行深度优先搜索时被访问的时间顺序，当然，你可以理解成一个序号（这个序号由小到大
+	int time = 0; // time when discover each vertex
+
+	private void dfs( int u, int[] low, int[] disc, List<Integer>[] graph, List<List<Integer>> res, int pre )
+	{
+		disc[ u ] = low[ u ] = ++time; // discover u
+		for ( int j = 0; j < graph[ u ].size(); j++ )
+		{
+			int v = graph[ u ].get( j );
+			if ( v == pre )
+			{
+				continue; // if parent vertex, ignore
+			}
+			if ( disc[ v ] == -1 )
+			{ // if not discovered
+				dfs( v, low, disc, graph, res, u );
+				/*Now if for an edge (u,v) if v node is not present in stack then it is a tree edge or
+				a neighboring edge. In such case, we update the low value for that particular node as :
+					if (Tree-Edge) then Low[u] = Min ( Low[u] , Low[v] ).*/
+				low[ u ] = Math.min( low[ u ], low[ v ] );
+
+				// critical
+				//节点 u 被访问的时间 disc[ u ]，要优先于（小于）以下这些节点被访问的时间 —— low[v] 。
+				//以节点 v 为根的搜索树中的所有节点
+				//通过一条非搜索树上的边，能够到达搜索树的所有节点
+				if ( low[ v ] > disc[ u ] )
+				{
+					// u - v is critical, there is no path for v to reach back to u or previous vertices of u
+					res.add( Arrays.asList( u, v ) );
+				}
+				/*We determine the head or start node of each SCC when we get a node whose Disc[u] = Low[u],
+				such a node is the head node of that SCC.
+				Every SCC should have such a node maintaining this condition. */
+				else if ( low[ u ] == disc[ u ] )
+				{
+					//Note: A Strongly Connected Component must have all its low values same.
+					System.out.print( "Strongly Connected Component: " );
+					for ( int i = 0; i < low.length; i++ )
+					{
+						if ( low[ i ] == low[ u ] )
+						{
+							System.out.print( i + " " );
+						}
+					}
+					System.out.println();
+				}
+			}
+			else
+			{ // if v discovered and is not parent of u, update low[u], cannot use low[v] because u is not subtree of v
+				/*So,If for an edge (u,v) if v node is already present in  disc, then it is a back edge and (u,v) pair
+				is Strongly Connected.  So we change the low value as :
+						if(Back-Edge) then Low[u] = Min ( Low[u] , Disc[v] ).
+
+				After visiting this node on returning the call to its parent node we will update the Low value
+				for each node to ensure that Low value remains the same for all nodes in the
+				Strongly Connected Component.*/
+				low[ u ] = Math.min( low[ u ], disc[ v ] );
+			}
+		}
+	}
+
+	List<List<Integer>> input = new ArrayList<>();
+
+	private void add( int a, int b )
+	{
+		List<Integer> l = new ArrayList<>();
+		l.add( a );
+		l.add( b );
+		input.add( l );
+	}
+
+	@Before
+	public void init()
+	{
+		add( 0, 1 );
+		add( 1, 2 );
+		add( 2, 3 );
+		add( 3, 4 );
+		add( 4, 0 );
+		add( 0, 5 );
+		add( 5, 6 );
+		add( 5, 7 );
+		add( 7, 8 );
+		add( 8, 5 );
+	}
+
+	@Test
+	public void test_tarjan()
+	{
+		var result = solution( 9, input );
+		System.out.println( "Critical Connections: " + result );
+	}
 }
--- src/amazon/reorderLogFiles.java
+++ src/amazon/reorderLogFiles.java
@@ -1,5 +1,11 @@
 package amazon;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeMap;
+
 /*
 *
 * 937. Reorder Data in Log Files
@@ -16,17 +22,14 @@ The letter-logs are sorted lexicographically by their contents. If their content
 The digit-logs maintain their relative ordering.
 Return the final order of the logs.
 
-
-
 Example 1:
-
 Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
 Explanation:
 The letter-log contents are all different, so their ordering is "art can", "art zero", "own kit dig".
 The digit-logs have a relative order of "dig1 8 1 5 1", "dig2 3 6".
-Example 2:
 
+* Example 2:
 Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
 Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
 
@@ -38,8 +41,133 @@ Constraints:
 All the tokens of logs[i] are separated by a single space.
 logs[i] is guaranteed to have an identifier and at least one word after the identifier.
 通过次数10,963提交次数18,622*/
-public class reorderLogFiles {
-    public String[] solution(String[] logs) {
-        return null;
-    }
+public class reorderLogFiles
+{
+	String[] input1 = { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero" };
+	String[] input2 = { "a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo" };
+
+	static class ParseResult
+	{
+		int location = 0;
+		boolean isDigit = false;
+		String id = null;
+		String content = null;
+
+		ParseResult( String log, int location )
+		{
+			//			int p = log.indexOf( " " );
+			//			if ( p > 0 )
+			//			{
+			//				this.location = location;
+			//				// can't find, ignore
+			//				id = log.substring( 0, p ).trim();
+			//				content = log.substring( p + 1 ).trim();
+			//				char firstChar = content.charAt( 0 );
+			//				isDigit = firstChar >= '0' && firstChar <= '9';
+			//			}
+
+			this.location = location;
+			String[] parts = log.split( " ", 2 );
+			id = parts[ 0 ];
+			content = parts[ 1 ];
+			isDigit = Character.isDigit( content.charAt( 0 ) );
+		}
+
+		ParseResult( String log )
+		{
+			this( log, 0 );
+		}
+
+		@Override
+		public String toString()
+		{
+			return "ParseResult{" +
+					"isDigit=" + isDigit +
+					", id='" + id + '\'' +
+					", content='" + content + '\'' +
+					'}';
+		}
+	}
+
+	/*Runtime: 4 ms, faster than 78.69% of Java online submissions for Reorder Data in Log Files.
+Memory Usage: 39.5 MB, less than 51.16% of Java online submissions for Reorder Data in Log Files.*/
+	public String[] solution( String[] logs )
+	{
+		// first create two array
+		TreeMap<ParseResult, String> map = new TreeMap<>( ( r1, r2 ) -> {
+			int result = 0;
+			if ( !r1.isDigit && !r2.isDigit )
+			{
+				result = r1.content.compareTo( r2.content );
+				if ( result == 0 )
+				{
+					result = r1.id.compareTo( r2.id );
+				}
+			}
+			else
+			{
+				result = r1.isDigit && !r2.isDigit ? 1 :
+						!r1.isDigit && r2.isDigit ? -1 : r1.location - r2.location;
+			}
+			return result;
+		} );
+		for ( int i = 0; i < logs.length; i++ )
+		{
+			String log = logs[ i ];
+			map.put( new ParseResult( log, i ), log );
+		}
+
+		return map.values().toArray( new String[ 0 ] );
+
+	}
+
+	public String[] solution_arraySort( String[] logs )
+	{
+		Comparator<String> comp = ( log1, log2 ) -> {
+			int result = 0;
+			ParseResult r1 = new ParseResult( log1 );
+			ParseResult r2 = new ParseResult( log2 );
+			if ( !r1.isDigit && !r2.isDigit )
+			{
+				result = r1.content.compareTo( r2.content );
+				if ( result == 0 )
+				{
+					result = r1.id.compareTo( r2.id );
+				}
+			}
+			else
+			{
+				result = r1.isDigit && !r2.isDigit ? 1 :
+						!r1.isDigit && r2.isDigit ? -1 : 0;
+			}
+			return result;
+		};
+		Arrays.sort( logs, comp );
+		return logs;
+	}
+
+	@Test
+	public void test_parser()
+	{
+		Arrays.stream( input1 ).forEach( log ->
+				System.out.println( new ParseResult( log ) ) );
+		Arrays.stream( input2 ).forEach( log ->
+				System.out.println( new ParseResult( log ) ) );
+	}
+
+	@Test
+	public void test_solution_treemap()
+	{
+		System.out.println( Arrays.toString( solution( input1 ) ) );
+		System.out.println( Arrays.toString( solution( input2 ) ) );
+	}
+
+	@Test
+	public void test_solution_arraysort()
+	{
+		// so here the arraysort is stable sorting, but treemap is not,
+		// we need to add the count to TreeMap sorting comparator
+		System.out.println( Arrays.toString( solution_arraySort( input1 ) ) );
+		System.out.println( Arrays.toString( solution_arraySort( input2 ) ) );
+	}
 }
