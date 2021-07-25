d5f21656f75fe0e8ad7a2f93d08f30c4246c294c
mauxmee <mauxmee@gmail.com>
2021-07-25 14:42:46 +1000

    trapping rain water ( copied from standard answer)

--- src/amazon/TrappingRainWater.java
+++ src/amazon/TrappingRainWater.java
@@ -1,21 +1,20 @@
 package amazon;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /*42. Trapping Rain Water
 Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
-
-
 Example 1:
-
-
 Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
-Example 2:
 
+Example 2:
 Input: height = [4,2,0,3,2,5]
 Output: 9
-
 
 Constraints:
 
@@ -25,7 +24,86 @@ n == height.length
 通过次数278,429提交次数491,790
 */
 public class TrappingRainWater {
-    public int trap(int[] height) {
-        return 0;
+    /*
+    * 对于下标 i，下雨后水能到达的最大高度等于下标 i 两边的最大高度的最小值，下标 i 处能接的雨水量等于下标 i 处的水能到达的最大高度减去 height[i]。
+
+朴素的做法是对于数组 height中的每个元素，分别向左和向右扫描并记录左边和右边的最大高度，然后计算每个下标位置能接的雨水量。假设数组 height 的长度为 n，该做法需要对每个下标位置使用 O(n) 的时间向两边扫描并得到最大高度，因此总时间复杂度是 O(n^2)
+上述做法的时间复杂度较高是因为需要对每个下标位置都向两边扫描。如果已经知道每个位置两边的最大高度，则可以在 O(n) 的时间内得到能接的雨水总量。使用动态规划的方法，可以在 O(n) 的时间内预处理得到每个位置两边的最大高度。
+
+创建两个长度为 n 的数组 leftMax 和 rightMax。对于 0≤i<n，leftMax[i] 表示下标 i 及其左边的位置中，height 的最大高度，rightMax[i] 表示下标 i 及其右边的位置中，height 的最大高度。
+
+显然，leftMax[0]=height[0]，rightMax[n−1]=height[n−1]。两个数组的其余元素的计算如下：
+当 1≤i≤n−1 时，leftMax[i]=max(leftMax[i−1],height[i])；
+当 0≤i≤n−2 时，rightMax[i]=max(rightMax[i+1],height[i])。
+*/
+    //时间复杂度：O(n)，其中 n 是数组 height 的长度。计算数组 leftMax 和 rightMax 的元素值各需要遍历数组 height 一次，计算能接的雨水总量还需要遍历一次。
+    //
+    //空间复杂度：O(n)，其中 n 是数组 height 的长度。需要创建两个长度为 n 的数组 leftMax 和 rightMax
+    public int trap1(int[] height) {
+        int n = height.length;
+        if (n == 0) return 0;
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i > 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            ret += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return ret;
+    }
+
+    /*
+    * 双指针
+动态规划的做法中，需要维护两个数组 leftMax 和 rightMax，因此空间复杂度是 O(n)。是否可以将空间复杂度降到 O(1)？
+注意到下标 i 处能接的雨水量由 leftMax[i] 和 rightMax[i] 中的最小值决定。由于数组 leftMax 是从左往右计算，
+* 数组 rightMax 是从右往左计算，因此可以使用双指针和两个变量代替两个数组。
+维护两个指针 left 和 right，以及两个变量 leftMax 和 rightMax，初始时 left=0,right=n−1,leftMax=0,rightMax=0。
+* 指针 left 只会向右移动，指针 right 只会向左移动，在移动指针的过程中维护两个变量 leftMax 和 rightMax 的值。
+当两个指针没有相遇时，进行如下操作：
+
+使用 height[left] 和 height[right] 的值更新 leftMax 和 rightMax 的值；
+如果 height[left]<height[right]，则必有 leftMax<rightMax，下标left 处能接的雨水量等于 leftMax−height[left]，
+* 将下标 left 处能接的雨水量加到能接的雨水总量，然后将 left 加 1（即向右移动一位）；
+如果 height[left]≥height[right]，则必有 leftMax≥rightMax，下标 right 处能接的雨水量等于 rightMax−height[right]，
+* 将下标 right 处能接的雨水量加到能接的雨水总量，然后将 right 减 1（即向左移动一位）。
+
+当两个指针相遇时，即可得到能接的雨水总量。
+*
+时间复杂度：O(n)，其中 n 是数组 height 的长度。两个指针的移动总次数不超过 n。
+空间复杂度：O(1)。只需要使用常数的额外空间。
+
+*/
+    public int trap2(int[] height) {
+        int ret = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ret += leftMax - height[left];
+                ++left;
+            } else {
+                ret += rightMax - height[right];
+                --right;
+            }
+        }
+        return ret;
+    }
+
+    int[] input1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+
+    @Test
+    public void test_solution1() {
+        assertEquals(trap1(input1), 6);
+        assertEquals(trap2(input1), 6);
     }
 }
